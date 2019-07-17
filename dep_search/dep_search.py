256b0f2c85903f88460057edfb8e9b7707a9773b
e_prudnikov <evgeniy.prudnikovv@yandex.ru>
2019-07-17 14:34:19 +0200

    upd

--- dep_search/dep_search.py
+++ dep_search/dep_search.py
@@ -2,12 +2,13 @@ import os
 import pickle
 import re
 
+INDEX = {}
 EXCLUDE_DIR_NAMES = ['scripts', 'json', 'dev_kiev', 'dev_kiev_pr']
 ACCEPTED_FILES_TYPES = ['.pkb', '.sql']
 
-trg_re = re.compile('@?insert@(.*?)@?into@([_a-zA-Z0-9]+?)[@|(]', re.DOTALL | re.MULTILINE | re.IGNORECASE)
-src_re = re.compile('@(from|inner@join|left@join|right@join|full@join|cross@join|join)@([_a-zA-Z0-9]+?)@', re.DOTALL | re.MULTILINE | re.IGNORECASE)
-src_with_catch = re.compile('@?(with|,)@([_a-zA-Z0-9]+?)@as@\(', re.DOTALL | re.MULTILINE | re.IGNORECASE)
+trg_re = re.compile('@?insert@(.*?)@?into@([\._a-zA-Z0-9]+?)[@|(]', re.DOTALL | re.MULTILINE)
+src_re = re.compile('@(from|inner@join|left@join|right@join|full@join|cross@join|join)@([\.\$_a-zA-Z0-9]+?)@', re.DOTALL | re.MULTILINE)
+src_with_catch = re.compile('@?(with|,)@([_a-zA-Z0-9]+?)@as@\(', re.DOTALL | re.MULTILINE)
 
 
 a = {'Python': '.py', 'C++': '.cpp', 'Java': '.java'}
@@ -23,7 +24,11 @@ lol_path = r''
 
 def clear_data(text):
     # lines clearing
-    text_lines = [line for line in text.split('\n') if line.strip() and not line.strip().startswith('--')]
+    text_lines = [line.strip().lower() for line in text.split('\n') if line.strip() and not line.strip().startswith('--')]
+    # print(text_lines)
+    if text_lines is None:
+        return None
+
     #  comments clearing
     cl_data = []
     is_multiline_comment = 0
@@ -54,10 +59,11 @@ def process_prefix(object_name):
 
 def process_file(file_path):
     ind_part = {}
-
-    f = open(file_path, 'r')
+    # if 'pkg_etl_f_bc_alg_ww' in file_path:
+    #     print(file_path)
+    f = open(file_path, 'rb')
     try:
-        data = f.read()
+        data = f.read().decode('utf-8', 'ignore')
     except Exception as e:
         print(e)
         print(file_path + '-- broken')
@@ -65,9 +71,13 @@ def process_file(file_path):
 
     for stm in data.split(';'):
         stm = stm.strip().lower()
-        if not (stm.startswith('insert') or stm.startswith('merge')):
-            continue
+        # if 'pkg_utl_truncate_table.truncate_subpart_table' in stm:
+        #     print('1')
         cl_data = clear_data(stm)
+        if cl_data:
+            if not (cl_data.startswith('insert') or cl_data.startswith('merge')):
+                continue
+
         cl_data = '@'.join(cl_data.split())
         trg_object = None
         try:
@@ -88,13 +98,10 @@ def process_file(file_path):
 
             ind_part[trg_object] = t_sources
 
-    print(ind_part)
+    # print(ind_part)
     f.close()
 
     return ind_part
-
-
-root_dir_path = r''
 
 
 def add_to_index(index, ind_part):
@@ -110,11 +117,11 @@ def add_to_index(index, ind_part):
                 res_val = tuple(set(index[k] + ind_part[k]))
                 index[k] = res_val
 
+    del ind_part
     # print(index)
 
 
 def create_index(root_dir_path, exclude_dir_names=None):
-    index = {}
     for path, subdirs, files in os.walk(root_dir_path):
         subdirs[:] = [d for d in subdirs if d not in exclude_dir_names]
         if not files:
@@ -124,11 +131,20 @@ def create_index(root_dir_path, exclude_dir_names=None):
             if not f.endswith('.pks'):
                 # print(os.path.join(path, f))
                 ind_part = process_file(os.path.join(path, f))
-                add_to_index(index, ind_part)
+                add_to_index(INDEX, ind_part)
 
-    print(index)
+    print(INDEX)
 
 
-# create_index(root_dir_path, EXCLUDE_DIR_NAMES)
+root_dir_path = r''
+
+create_index(root_dir_path, EXCLUDE_DIR_NAMES)
+
+import sys
+print(sys.getsizeof(INDEX))
 
 # process_file(lol_path)
+
+18528
+
+txt = input("wait please")
